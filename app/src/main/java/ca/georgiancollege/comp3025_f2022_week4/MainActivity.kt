98e8e8ba1afbf8eab99177bc38191f63e049c1bd
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-09-30 09:43:26 -0400

    enabled number button input v1

--- app/src/main/java/ca/georgiancollege/comp3025_f2022_week4/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/comp3025_f2022_week4/MainActivity.kt
@@ -6,33 +6,56 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    var ResultLabel: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val ResultLabel = findViewById(R.id.Result_TextView) as TextView
-
-
-        // named event handler
-        fun NumberButtons(view: View)
-        {
-            val buttonInfo = view as Button
-
-            println(buttonInfo.text)
-        }
-
-        fun OperatorButtons(view: View)
-        {
-            val buttonInfo = view as Button
-
-            println(buttonInfo.text)
-        }
-
-
-        findViewById<Button>(R.id.Clear_Button).setOnClickListener { view -> OperatorButtons(view) }
-        findViewById<Button>(R.id.Plus_Minus_Button).setOnClickListener { view -> OperatorButtons(view) }
-        findViewById<Button>(R.id.Percent_Button).setOnClickListener { view -> OperatorButtons(view) }
-        findViewById<Button>(R.id.Divide_Button).setOnClickListener { view -> OperatorButtons(view) }
+        ResultLabel = findViewById<TextView>(R.id.Result_TextView)
     }
+
+    fun NumberButtons(view: View)
+    {
+        val buttonInfo = view as Button
+        val buttonText = buttonInfo.text
+        if(ResultLabel?.text == "0")
+        {
+            ResultLabel?.text = buttonText
+        }
+        else
+        {
+            if (buttonText == ".")
+            {
+                if(ResultLabel?.text?.contains(".") != true)
+                {
+                    ResultLabel?.text = ResultLabel?.text.toString().plus(buttonText)
+                }
+            }
+            else
+            {
+                ResultLabel?.text = ResultLabel?.text.toString().plus(buttonText)
+            }
+        }
+
+
+    }
+
+    fun OperatorButtons(view: View)
+    {
+        val buttonInfo = view as Button
+
+
+    }
+
+    fun ExtraButtons(view: View)
+    {
+        val buttInfo = view as Button
+
+
+    }
+
 }
